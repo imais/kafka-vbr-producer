f40f0dc1714fc2ece296d0f50b9aaa35c62ee11a
Shigeru Imai <shigeru322@gmail.com>
2018-01-03 03:07:24 -0500

    Add support for throughput_control_mode

--- producer.c
+++ producer.c
@@ -1,3 +1,4 @@
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -31,10 +32,34 @@ typedef enum {
 static char *throughput_unit_str[4] = {"bytes", "kbytes", "mbytes", "gbytes"};
 static int throughput_unit_conversion[4] = {1, 1e3, 1e6, 1e9};
 
+typedef enum {
+    THROUGHPUT_FILE = 0,
+    REPLAY_ADSB,
+    REPLAY_TWEETS,
+    REPLAY_WCUP,
+    NUM_THROUGHPUT_CONTROL_MODES
+} throughput_control_mode_t;
+static char *throughput_control_mode_str[4] =
+    {"throughput_file", "replay_adsb", "replay_tweets", "replay_wcup"};
+/* Delivery report callbacks */
+static void dr_msg_throughput_file_cb (rd_kafka_t *, const rd_kafka_message_t *, void *);
+static void dr_msg_replay_cb (rd_kafka_t *, const rd_kafka_message_t *, void *);
+static void (*dr_msg_cbs[NUM_THROUGHPUT_CONTROL_MODES])
+    (rd_kafka_t*, const rd_kafka_message_t*, void *) = 
+         {dr_msg_throughput_file_cb, dr_msg_replay_cb, dr_msg_replay_cb, dr_msg_replay_cb};
+/* Read message functions */
+static int read_msg (char *);
+static int read_adsb_msg (char *);
+static int read_tweets_msg (char *);
+static int read_wcup_msg (char *);
+static int (*read_msg_funcs[NUM_THROUGHPUT_CONTROL_MODES]) (char *) = 
+    {read_msg, read_adsb_msg, read_tweets_msg, read_wcup_msg};
+
 typedef struct {
     char *brokers;              /* CSV list of brokers (host:port) */
     char *topic;
     char *batch_num_messages;
+    throughput_control_mode_t throughput_control_mode;
     throughput_unit_t throughput_unit;
     int throughput_interval_sec;
     char *throughput_file;
@@ -46,7 +71,7 @@ static char *message_loop_str[2] = {"false", "true"};
 /* global variables */
 static producer_conf_t prod_conf;
 static int run = 1;
-static long time_start = 0, time_lastcheck = 0, time_status_lastprint = 0;
+static long time_start = 0, time_last_produce = 0, time_last_check = 0, time_last_print_status = 0;
 static long bytes_sent = 0;
 static int msgs_sent = 0;
 static FILE *throughput_fp = NULL, *message_fp = NULL;
@@ -67,6 +92,8 @@ static
 void print_config(producer_conf_t *conf) {
     printf("Producer configurations:\n");
     printf("\tbrokers=%s, topic=%s\n", conf->brokers, conf->topic);
+    printf("\tbatch_num_messagess=%s\n", conf->batch_num_messages);
+    printf("\tthroughput_control_mode=%s\n", throughput_control_mode_str[conf->throughput_control_mode]);
     printf("\tthroughput_unit=%s, throughput_file=%s\n",
            throughput_unit_str[conf->throughput_unit], conf->throughput_file);
     printf("\tthroughput_interval_sec=%d\n", conf->throughput_interval_sec);
@@ -84,6 +111,20 @@ int parse_config(void* user, const char* section, const char* name, const char* 
         conf->topic = strdup(value);
     else if (MATCH("kafka", "batch.num.messages"))
         conf->batch_num_messages = strdup(value);
+    else if (MATCH("throughput", "control_mode")) {
+        if (strcasecmp(value, "throughput_file")) 
+            conf->throughput_control_mode = THROUGHPUT_FILE;
+        else if (strcasecmp(value, "replay_adsb"))
+            conf->throughput_control_mode = REPLAY_ADSB;
+        else if (strcasecmp(value, "replay_tweets"))
+            conf->throughput_control_mode = REPLAY_TWEETS;
+        else if (strcasecmp(value, "replay_wcup"))
+            conf->throughput_control_mode = REPLAY_WCUP;
+        else {
+            fprintf(stderr, "%% Illegal throughput control option: %s\n", optarg);
+            goto illegal_config;            
+        }
+    }
     else if (MATCH("throughput", "unit")) {
         if (strcasecmp(value, "bytes") == 0 || strcasecmp(value, "b") == 0)
             conf->throughput_unit = BYTES;
@@ -160,7 +201,7 @@ static float get_throughput_target(long time_curr) {
     return throughput_target;
 }
 
-static int read_message(char *buf) {
+static int read_msg(char *buf) {
     while (fgets(buf, MSG_BUFLEN, message_fp) == NULL) {
         if (feof(message_fp) && prod_conf.message_loop) {
             fprintf(stderr, "%% Message file reached eof, rewinded to the beginning of the file\n");
@@ -174,8 +215,20 @@ static int read_message(char *buf) {
     return strlen(buf);
 }
 
+static int read_adsb_msg(char *buf) {
+    return 0;
+}
+
+static int read_tweets_msg(char *buf) {
+    return 0;
+}
+
+static int read_wcup_msg(char *buf) {
+    return 0;
+}
+
 static 
-void msg_callback (rd_kafka_t *rk, const rd_kafka_message_t *rkmessage, void *opaque) {
+void dr_msg_throughput_file_cb (rd_kafka_t *rk, const rd_kafka_message_t *rkmessage, void *opaque) {
     /* NOTE: This function is called in the context of main thread */
     long time_curr, time_delta, time_wait;
     float throughput_target, throughput_curr, throughput_adjusted;
@@ -189,7 +242,7 @@ void msg_callback (rd_kafka_t *rk, const rd_kafka_message_t *rkmessage, void *op
         bytes_sent += rkmessage->len;
         msgs_sent++;
         time_curr = get_current_time_msec();
-        time_delta = time_curr - time_lastcheck;
+        time_delta = time_curr - time_last_check;
 
         if (time_delta >= THROUGHPUT_CHECK_INTERVAL_MSEC) {
             if ((throughput_target = get_throughput_target(time_curr)) < 0) {
@@ -203,17 +256,21 @@ void msg_callback (rd_kafka_t *rk, const rd_kafka_message_t *rkmessage, void *op
                 time_wait = bytes_sent / throughput_target - time_delta;
                 /* printf("Sent %d msgs (%ld bytes) in %ld ms, tp(tgt: %.2f, cur: %.2f bytes/s), t_wait = %ld ms\n", msgs_sent, bytes_sent, time_delta, throughput_target, throughput_curr, time_wait); */
                 usleep(time_wait * 1000);
-                throughput_adjusted = (float)bytes_sent / (get_current_time_msec() - time_lastcheck);
+                throughput_adjusted = (float)bytes_sent / (get_current_time_msec() - time_last_check);
                 printf("Throughput: target %.2f vs. adjusted %.2f bytes/s\n",
                        throughput_target, throughput_adjusted);
             }
             bytes_sent = 0;
             msgs_sent = 0;
-            time_lastcheck = time_curr;
+            time_last_check = time_curr;
         }
     }
 
     /* The rkmessage is destroyed automatically by librdkafka */
+}
+
+static 
+void dr_msg_replay_cb (rd_kafka_t *rk, const rd_kafka_message_t *rkmessage, void *opaque) {
 }
 
 
@@ -225,11 +282,6 @@ int main (int argc, char** argv) {
     char msgbuf[MSG_BUFLEN];    /* Message value temporary buffer */
     int retval = EXIT_SUCCESS;
     long time_curr;
-
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s [.ini file]\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
 
     printf("main thread ID: %u\n", (unsigned int)pthread_self());
 
@@ -243,10 +295,11 @@ int main (int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
     print_config(&prod_conf);
-    time_start = time_lastcheck = get_current_time_msec();
+    time_start = time_last_check = get_current_time_msec();
     
     /* Open throughput and message files */
-    if ((throughput_fp = fopen(prod_conf.throughput_file, "r")) == NULL) {
+    if (prod_conf.throughput_control_mode == THROUGHPUT_FILE &&
+        (throughput_fp = fopen(prod_conf.throughput_file, "r")) == NULL) {
         fprintf(stderr, "%% Failed opening %s\n", prod_conf.throughput_file);
         retval = EXIT_FAILURE;
         goto exit;
@@ -271,7 +324,7 @@ int main (int argc, char** argv) {
         retval = EXIT_FAILURE;
         goto exit;
     }
-    rd_kafka_conf_set_dr_msg_cb(kafka_conf, msg_callback);
+    rd_kafka_conf_set_dr_msg_cb(kafka_conf, dr_msg_cbs[prod_conf.throughput_control_mode]);
     
     /* Create main Kafka object */
     rk = rd_kafka_new(RD_KAFKA_PRODUCER, kafka_conf, errstr, sizeof(errstr));
@@ -291,7 +344,7 @@ int main (int argc, char** argv) {
     }
 
     while (run) {
-        size_t len = read_message(msgbuf);
+        size_t len = read_msg_funcs[prod_conf.throughput_control_mode](msgbuf);
         if (len == 0) {
             fprintf(stderr, "%% Finished reading all messages");
             break;
@@ -299,10 +352,10 @@ int main (int argc, char** argv) {
 
         #if 0
         time_curr = get_current_time_msec();        
-        if (time_curr - time_status_lastprint > STATUS_PRINT_INTERVAL_SEC * 1000) {
+        if (time_curr - time_last_print_status > STATUS_PRINT_INTERVAL_SEC * 1000) {
             printf("time=%ld, msgs_sent_total=%d, bytes_sent_total=%ld\n", 
                    time_curr, msgs_sent_total, bytes_sent_total);
-            time_status_lastprint = time_curr;
+            time_last_print_status = time_curr;
         }
         #endif
 
